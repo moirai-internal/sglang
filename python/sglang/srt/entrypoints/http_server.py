e9b90f415ce96caddd3aed178870d6b19c83dd84
Chang Su <iamchangsu96@gmail.com>
2025-09-19 12:00:39 -0700

    Cherry-pick: features-based-on-v0.5.1.post3 → features-based-on-v0.5.2 (#29)
    Cherry-pick oci specific commits into features-based-on-v0.5.1.post3 (#24)
    
    Cherry-pick conflicts: features-based-on-v0.4.10.post2 → features-based-on-v0.5.0rc1 (#18)
    
    * Cherry-pick conflicts: features-based-on-v0.4.7.post1 → features-based-on-v0.4.10.post2 (#10)
    
    * [Docker] Use local source file instead of git clone sglang (#4)
    
    * ✅ Resolved: Cherry-pick
    
    ---------
    
    Co-authored-by: Chao Yang <chao.c.yang@oracle.com>
    Co-authored-by: Simo Lin <linsimo.mark@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- python/sglang/srt/entrypoints/http_server.py
+++ python/sglang/srt/entrypoints/http_server.py
@@ -65,6 +65,7 @@ from sglang.srt.entrypoints.openai.serving_completions import OpenAIServingCompl
 from sglang.srt.entrypoints.openai.serving_embedding import OpenAIServingEmbedding
 from sglang.srt.entrypoints.openai.serving_rerank import OpenAIServingRerank
 from sglang.srt.entrypoints.openai.serving_score import OpenAIServingScore
+from sglang.srt.entrypoints.server_middlewares import OpcRequestIdMiddleware
 from sglang.srt.function_call.function_call_parser import FunctionCallParser
 from sglang.srt.managers.io_struct import (
     AbortReq,
@@ -295,6 +296,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(OpcRequestIdMiddleware)
 
 
 @app.exception_handler(HTTPException)
